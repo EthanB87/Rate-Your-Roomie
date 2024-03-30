3e5bf06a5853f4c3194ab747a7664cd5e2f2d004
EthanB87 <ethan@brockmanfamily.ca>
2024-03-30 16:26:08 -0400

    Initial Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {HomePageComponent} from "./pages/home-page/home-page.component";
+import {AddReviewComponent} from "./pages/add-review/add-review.component";
+import {AllUsersComponent} from "./pages/all-users/all-users.component";
+import {SearchComponent} from "./pages/search/search.component";
+import {NewUserComponent} from "./pages/new-user/new-user.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "home", component: HomePageComponent},
+  {path: "add", component: AddReviewComponent},
+  {path: "all/:id", component: AllUsersComponent},
+  {path: "", redirectTo: 'search', pathMatch: "full"},
+  {path: "search", component: SearchComponent},
+  {path: "new", component: NewUserComponent},
+];
