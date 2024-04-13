87b14f451f77acc3dd897c090e5f420f2b04a892
EthanB87 <ethan@brockmanfamily.ca>
2024-04-13 15:20:25 -0400

    Added validation to add user page

--- src/app/pages/new-user/new-user.component.ts
+++ src/app/pages/new-user/new-user.component.ts
@@ -1,5 +1,5 @@
 import {Component, inject} from '@angular/core';
-import {FormBuilder, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {NavbarComponent} from "../../partials/navbar/navbar.component";
 import {Roommate} from "../../../models/Roommate.Model";
 import {RoommateDALService} from "../../../services/roommate-dal.service";
@@ -21,17 +21,34 @@ export class NewUserComponent {
   dal = inject(RoommateDALService);
   fb = inject(FormBuilder);
 
+newUserForm = this.fb.group({
+  firstName: ["", [Validators.required, Validators.minLength(2), Validators.maxLength(16)]],
+  lastName: ["", [Validators.required, Validators.minLength(2), Validators.maxLength(16)]],
+  dob: ["", [Validators.required]],
+  schoolName: ["", [Validators.required, Validators.minLength(10), Validators.maxLength(25)]],
+  gender: ["", [Validators.required]]
+});
+
+refFirst = this.newUserForm.controls["firstName"];
+refLast = this.newUserForm.controls["lastName"];
+refDob = this.newUserForm.controls["dob"];
+refSchool = this.newUserForm.controls["schoolName"];
+refGender = this.newUserForm.controls["gender"];
 
   onSubmit(): void {
-    // Insert roommate review into the database
-    this.dal.insert(this.roommate)
-      .then(() => {
-        console.log('Roommate added successfully');
-        this.resetForm();
-      })
-      .catch(e => {
-        console.error('Error adding roommate:', e);
-      });
+    if(this.newUserForm.valid){
+      // Insert roommate review into the database
+      this.dal.insert(this.roommate)
+        .then(() => {
+          console.log('Roommate added successfully');
+          this.resetForm();
+        })
+        .catch(e => {
+          console.error('Error adding roommate:', e);
+        });
+    } else{
+      alert("Please Fill Out All Form Fields");
+    }
   }
 
   resetForm(): void {
